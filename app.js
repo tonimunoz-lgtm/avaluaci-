2d04fd3f97dbc11e05e374ed00e5021449b51295
tonimunoz-lgtm <toni.munoz@institutmatadepera.cat>
2025-11-15 09:36:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -143,14 +143,14 @@ function loadClassesScreen(){
   screenClasses.classList.remove('hidden');
   classesGrid.innerHTML = '<div class="col-span-full text-sm text-gray-500">Carregant...</div>';
 
-  const classColors = [
-    'from-indigo-400 to-purple-500',
-    'from-pink-400 to-red-500',
-    'from-yellow-400 to-orange-500',
-    'from-green-400 to-teal-500',
-    'from-blue-400 to-indigo-500',
-    'from-purple-400 to-pink-500'
-  ];
+const classColors = [
+  'from-indigo-600 to-purple-600',
+  'from-pink-500 to-red-500',
+  'from-yellow-400 to-orange-500',
+  'from-green-500 to-teal-500',
+  'from-blue-500 to-indigo-600',
+  'from-purple-500 to-pink-500'
+];
 
   db.collection('professors').doc(professorUID).get().then(doc => {
     if(!doc.exists) { classesGrid.innerHTML = '<div class="text-sm text-red-500">Professor no trobat</div>'; return; }
