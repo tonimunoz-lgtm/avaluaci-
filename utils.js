c0f07a4b09f6454a01336b59add8e3c83b1d11e8
tonimunoz-lgtm <toni.munoz@institutmatadepera.cat>
2025-11-19 18:35:38 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,10 @@
+export function calculateAverage(grades) {
+  if (!grades.length) return 0;
+  return grades.reduce((a,b)=>a+b,0)/grades.length;
+}
 
+export function gradeColor(value) {
+  if (value >= 7) return 'bg-green-100';
+  if (value >= 5) return 'bg-yellow-100';
+  return 'bg-red-100';
+}
