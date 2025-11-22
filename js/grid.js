31a6f7c95eeccd269e836e540160788fec8268b2
tonimunoz-lgtm <toni.munoz@institutmatadepera.cat>
2025-11-22 16:05:30 +0100

    Create grid.js

---
+++ js/grid.js
@@ -0,0 +1,9 @@
+export function initGrid() {
+  // Inicialització, listeners de botons
+}
+
+export function openGrid(classId, groupId) {
+  document.getElementById("screen-groups").classList.add("hidden");
+  document.getElementById("appRoot").classList.remove("hidden");
+  // Carrega activitats del grup
+}
