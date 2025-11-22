d8ab829fdc8e50129fa030f1e0c441e20d9d6931
tonimunoz-lgtm <toni.munoz@institutmatadepera.cat>
2025-11-22 09:17:30 +0100

    Delete rounding.js

--- rounding.js
+++
@@ -1,6 +0,0 @@
-// rounding.js
-export function applyRounding(value, multiplier = 1) {
-  if (multiplier === 1) return Math.round(value);
-  if (multiplier === 0.5) return Math.round(value * 2) / 2;
-  return value; // per defecte, sense canvi
-}
