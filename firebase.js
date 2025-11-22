ecd82aeeff13138d559300ce96885f675ee2825c
tonimunoz-lgtm <toni.munoz@institutmatadepera.cat>
2025-11-22 09:02:51 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,19 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+// firebase.js - configuració Firebase i instàncies
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
-export const firebaseConfig = { ... }; // copia exacta del teu config
+const firebaseConfig = {
+  apiKey: "AIzaSyA0P7TWcEw9y9_13yqRhvsgWN5d3YKH7yo",
+  authDomain: "gestornotes-cc6d0.firebaseapp.com",
+  projectId: "gestornotes-cc6d0",
+  storageBucket: "gestornotes-cc6d0.firebasestorage.app",
+  messagingSenderId: "324570393360",
+  appId: "1:324570393360:web:13a65fcf948813805c5395"
+};
+
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
 export const db = firebase.firestore();
+export default firebase;
