cdd7d0cc00dc9f7a259a80fdb5434f7ec50108aa
tonimunoz-lgtm <toni.munoz@institutmatadepera.cat>
2025-11-29 13:27:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1683,3 +1683,19 @@ termMenu.querySelector('.delete-term-btn').addEventListener('click', async () =>
 
   termMenu.classList.add('hidden');
 });
+
+// COPIAR estructura
+termMenu.querySelector('.copy-term-structure-btn')
+  .addEventListener('click', () => {
+    const termId = Terms.getActiveTermId();
+    Terms.copyGridStructure(termId);
+    termMenu.classList.add('hidden');
+  });
+
+// ENGANXAR estructura
+termMenu.querySelector('.paste-term-structure-btn')
+  .addEventListener('click', async () => {
+    const termId = Terms.getActiveTermId();
+    await Terms.pasteGridStructure(termId);
+    termMenu.classList.add('hidden');
+  });
